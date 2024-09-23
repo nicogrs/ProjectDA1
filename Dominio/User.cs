d904b672772608b16eaf21c56de1ad7225951e7a
Nicolas <nicogrs20@gmail.com>
2024-09-22 21:22:31 -0300

    GREEN: Pasó test ValidBirthDate

--- Dominio/User.cs
+++ Dominio/User.cs
@@ -25,7 +25,7 @@ public class User
 
     public bool IsBirthDateValid()
     {
-        return false;
+        return BirthDate != null && BirthDate < DateTime.Today && BirthDate.Year < DateTime.Now.Year;
     }
 
 }

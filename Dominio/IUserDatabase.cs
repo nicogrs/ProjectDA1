afe45f8becd4f3bf0d324b9359264589b0c2aa48
Nicolas <nicogrs20@gmail.com>
2024-09-23 02:01:12 -0300

    Se continuan con los test de User. En este caso se implementan los de UserService mediante la clase UserListTest

---
+++ Dominio/IUserDatabase.cs
@@ -0,0 +1,10 @@
+﻿namespace Dominio;
+
+public interface IUserDatabase
+{
+    User GetUserByEmail(string email);
+    IEnumerable<User> GetAllUsers();
+    bool AddUser(User user);
+    bool UpdateUser(User user);
+    bool DeleteUser(string email);
+}
---
+++ Test/UserListTest.cs
@@ -0,0 +1,44 @@
+﻿namespace Test;
+using Dominio;
+using Moq;
+
+[TestClass]
+public class UserListTest
+{
+    private UserService _service;
+    private User _user;
+    Mock<IUserDatabase> _mockUserDatabase;
+    
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockUserDatabase = new Mock<IUserDatabase>();
+        _service = new UserService(_mockUserDatabase.Object);
+        _user = new User        
+        {
+            Name = "Carlos",
+            Surname = "Lopez",
+            Email = "carlos@gmail.com",
+            BirthDate = new DateTime(1980, 1, 1),
+            Password = "123456"
+        };
+
+    }
+    
+    [TestMethod]
+    public void AddOneUser()
+    {
+        _mockUserDatabase.Setup(x => x.GetUserByEmail(_user.Email)).Returns(_user);
+        var isUserAdded = _service.UserExists(_user.Email);
+        Assert.IsTrue(isUserAdded);
+    }
+
+    [TestMethod]
+    public void UserNotExists()
+    {
+        _mockUserDatabase.Setup(x => x.GetUserByEmail(_user.Email)).Returns((User)null);
+        var isUserDeleted = _service.UserExists(_user.Email);
+        Assert.IsFalse(isUserDeleted);
+    }
+    
+}

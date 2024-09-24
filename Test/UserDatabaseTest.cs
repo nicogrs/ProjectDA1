ea7e32b6dadebbc3cb3afdbd66efa06369aff1fa
Nicolas <nicogrs20@gmail.com>
2024-09-23 21:15:54 -0300

    GREEN: Pasa test AddUser de UserDatabaseTest

---
+++ Test/UserDatabaseTest.cs
@@ -0,0 +1,33 @@
+﻿namespace Test;
+using Dominio;
+using Dominio.Data;
+
+[TestClass]
+public class UserDatabaseTest
+{
+    private UserDataBase db;
+    private User _user;
+    
+    [TestInitialize]
+    
+    public void Setup()
+    {
+        db = new UserDataBase();
+        _user = new User        
+        {
+            Name = "Carlos",
+            Surname = "Lopez",
+            Email = "carlos@gmail.com",
+            BirthDate = new DateTime(1980, 1, 1),
+            Password = "123456"
+        };
+    }
+
+    [TestMethod]
+    public void AddUserToDataBase()
+    {
+        db.AddUser(_user);
+        var result = db.GetUserByEmail(_user.Email);
+        Assert.AreEqual(_user, result);
+    }
+}

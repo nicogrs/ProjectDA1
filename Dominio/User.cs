b45d9abec3dc53485c9c3f833dd8bace9affc799
Nicolas <nicogrs20@gmail.com>
2024-09-28 15:55:56 -0300

    Se crea la clase Team, para que compile y agregar la lista de Teams a User

--- Dominio/User.cs
+++ Dominio/User.cs
@@ -8,6 +8,8 @@ public class User
     public DateTime BirthDate { get; set; }
     public string Password { get; set; }
     public bool Admin { get; set; }
+    
+    public Team Teams { get; set; }
 
     public bool IsNameValid()
     {

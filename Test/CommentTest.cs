238d9843305a32c21665f3700628596ed399c168
rpresno <rjpresno@gmail.com>
2024-09-29 18:11:58 -0300

    Clase Comment, CommentTest

---
+++ Test/CommentTest.cs
@@ -0,0 +1,17 @@
+﻿using Dominio;
+namespace Test;
+
+
+[TestClass]
+public class CommentTest
+{
+    private User u1;
+    private User u2;
+    
+    [TestInitialize]
+    public void Setup()
+    {
+        u1 = new User();
+        u2 = new User();
+    }
+}

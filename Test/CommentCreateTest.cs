dba440b6f3a2f99b49879a286bdc9130f9905c18
rpresno <rjpresno@gmail.com>
2024-10-02 19:33:54 -0300

    Merge Feature-Comment

---
+++ Test/CommentCreateTest.cs
@@ -0,0 +1,31 @@
+﻿using Dominio;
+namespace Test;
+
+
+[TestClass]
+public class CommentCreateTest
+{
+    private User u1;
+    private User u2;
+    private Comment c;
+    
+    [TestInitialize]
+    public void Setup()
+    {
+        u1 = new User();
+        u2 = new User();
+        c = new Comment(u1, "ComentarioPrueba");
+    }
+
+    [TestMethod]
+    public void CommentCreateCreatedBy()
+    {
+        Assert.AreSame(c.CreatedBy, u1);
+    }
+    
+    [TestMethod]
+    public void CommentCreateContent()
+    {
+        Assert.AreSame(c.Content, "ComentarioPrueba");
+    }
+}
---
+++ dominio/Comment.cs
@@ -0,0 +1,17 @@
+﻿namespace Dominio;
+
+public class Comment
+{
+    public User CreatedBy {get; set;}
+    public bool Resolved {get; set;}
+    public User ResolvedBy {get; set;}
+    public string Content {get; set;}
+
+    public Comment(User createdBy, string content)
+    {
+        CreatedBy = createdBy;
+        Content = content;
+        Resolved = false;
+        ResolvedBy = null;
+    }
+}

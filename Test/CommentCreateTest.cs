456bf4e794fa06d1f451d34930a4af5879424174
rpresno <rjpresno@gmail.com>
2024-10-02 19:05:11 -0300

    REFACTOR, GREEN: CommentCreateTest cleanup

--- Test/CommentCreateTest.cs
+++ Test/CommentCreateTest.cs
@@ -7,25 +7,25 @@ public class CommentCreateTest
 {
     private User u1;
     private User u2;
+    private Comment c;
     
     [TestInitialize]
     public void Setup()
     {
         u1 = new User();
         u2 = new User();
+        c = new Comment(u1, "ComentarioPrueba");
     }
 
     [TestMethod]
     public void CommentCreateCreatedBy()
     {
-        Comment c = new Comment(u1, "ComentarioPrueba");
         Assert.AreSame(c.CreatedBy, u1);
     }
     
     [TestMethod]
     public void CommentCreateContent()
     {
-        Comment c = new Comment(u1, "ComentarioPrueba");
         Assert.AreSame(c.Content, "ComentarioPrueba");
     }
 }

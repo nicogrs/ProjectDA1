5522adf0b2af5cd27e236fa700070aab27524b8f
rpresno <rjpresno@gmail.com>
2024-10-02 21:41:05 -0300

    REFACTOR prioridad a enum

--- dominio/Task.cs
+++ dominio/Task.cs
@@ -3,9 +3,16 @@
 public class Task
 {
     public string Title { get; set; }
-    public string priority { get; set; }
+    public Priority priority { get; set; }
     public string Description { get; set; }
     public DateTime expDate { get; set; }
     public List<Comment> comments { get; set; }
-    
+
+
+    public enum Priority
+    {
+        Low,
+        Medium,
+        Urgent
+    }
 }

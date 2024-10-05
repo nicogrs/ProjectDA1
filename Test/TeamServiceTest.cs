9a52b8fe733f4b0f83bdac05f5fd348d3d67feb8
Nicolas <nicogrs20@gmail.com>
2024-10-05 17:32:18 -0300

    GREEN: Se crean test para verificar los metodos del constructor

---
+++ Test/TeamServiceTest.cs
@@ -0,0 +1,29 @@
+﻿namespace Test;
+using Dominio;
+
+[TestClass]
+
+public class TeamServiceTest
+{
+    private Team team;
+    
+    [TestInitialize]
+    public void Setup()
+    {
+        team = new Team
+        {
+            Name = "Team Example",
+            CreatedOn = new DateTime(2020, 05, 05),
+            TasksDescription = "Tareas sobre desarrollo",
+            MaxUsers = 5,
+            Panels = new List<Panel>(),
+        };
+    }
+
+    [TestMethod]
+    public void CreateTeam()
+    {
+        
+    }
+    
+}

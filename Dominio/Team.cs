57b2b37387407a48ee7e4e57c14dfdadd011b260
Nicolas <nicogrs20@gmail.com>
2024-10-05 01:20:52 -0300

    GREEN: Pasa test CreateTeam

--- Dominio/Team.cs
+++ Dominio/Team.cs
@@ -2,4 +2,9 @@
 
 public class Team
 {
+    public string Name { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string TasksDescription { get; set; }
+    public int MaxUsers { get; set; }
+    public List<Panel> Panels { get; set; }
 }

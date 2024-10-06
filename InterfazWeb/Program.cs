a2a717f09c1bdbbcc8d34907ca9358fa9d0a789f
Florencia <FB243045@fi365.or.edu.uy>
2024-10-06 02:43:28 -0300

    Se sube Program.cs actualizado

--- InterfazWeb/Program.cs
+++ InterfazWeb/Program.cs
@@ -1,6 +1,5 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using InterfazWeb.Data;
+using Dominio.Data;
+using Logica;
 
 namespace InterfazWeb;
 
@@ -9,12 +8,17 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
         // Add services to the container.
         builder.Services.AddRazorPages();
         builder.Services.AddServerSideBlazor();
-        builder.Services.AddSingleton<WeatherForecastService>();
 
+        builder.Services.AddSingleton<UserDataBase>();
+        builder.Services.AddSingleton<TeamDatabase>();
+        builder.Services.AddSingleton<PanelDatabase>();
+        builder.Services.AddSingleton<TaskDatabase>();
+        builder.Services.AddSingleton<CommentDatabase>();
+        builder.Services.AddSingleton<Session>();
+        
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.

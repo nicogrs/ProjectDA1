9c6adc1de3ae303b4579a8f16dd77b940e2fffc0
Florencia <FB243045@fi365.or.edu.uy>
2024-10-06 02:15:06 -0300

    Se sube clase Session

---
+++ Bussines/Session.cs
@@ -0,0 +1,39 @@
+﻿using Dominio.Data;
+using Task = Dominio.Task;
+
+namespace Logica;
+using Dominio;
+
+public class Session
+{
+    public User CurrentUser { get; private set; }
+    public bool IsAdmin => CurrentUser != null && CurrentUser.Admin;
+
+    public void Login(User user)
+    {
+        CurrentUser = user;
+    }
+
+    public void Logout()
+    {
+        CurrentUser = null;
+    }
+
+
+    public List<Team> GetTeams()
+    {
+        return CurrentUser?.Teams ?? new List<Team>();
+    }
+    public List<Panel> GetPanels(string teamName)
+    {
+        var team = CurrentUser?.Teams.FirstOrDefault(t => t.Name == teamName);
+        return team?.Panels ?? new List<Panel>();
+    }
+    
+    public List<Task> GetTasks(string teamName, string panelName)
+    {
+        var team = CurrentUser?.Teams.FirstOrDefault(t => t.Name == teamName);
+        var panel = team?.Panels.FirstOrDefault(p => p.Name == panelName);
+        return panel?.Tasks ?? new List<Task>();
+    }
+}

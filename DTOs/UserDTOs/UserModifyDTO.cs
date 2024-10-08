a87d2392fa172b09ae47e7fbb359b3615b821580
Florencia <FB243045@fi365.or.edu.uy>
2024-10-08 08:22:20 -0300

    Se sube BM de usuarios

---
+++ DTOs/UserDTOs/UserModifyDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Dominio;
+namespace DTOs;
+
+public class UserModifyDTO
+{
+
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "El nombre debe tener entre 2 y 100 caracteres")]
+    public string Name { get; set; }
+    
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "El apellido debe tener entre 2 y 100 caracteres")]
+    public string Surname { get; set; }
+    
+    [EmailAddress(ErrorMessage = "Formato de email inválido")]
+    public string Email { get; set; }
+    
+    [DataType(DataType.Date)]
+    public DateTime BirthDate { get; set; }
+    
+    public string Password { get; set; }
+    
+    public bool Admin { get; set; }
+    
+}

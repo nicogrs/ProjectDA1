697df02dd573cda84b36538c3e2d8fbf7b243842
Nicolas <nicogrs20@gmail.com>
2024-10-09 20:30:46 -0300

    RED: Fallo test GetTeamByName

--- Dominio/Data/TeamDataBase.cs
+++ Dominio/Data/TeamDataBase.cs
@@ -1,6 +1,34 @@
 ﻿namespace Dominio.Data;
 
-public class TeamDataBase
+public class TeamDataBase : ITeamDatabase
 {
-    
+    public Team GetTeamByName(string teamName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddTeamToDatabase(Team team)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveTeamFromDatabase(string teamName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateTeamInDatabase(Team team)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Team> GetTeamsByUserEmail(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Team> GetTeams()
+    {
+        throw new NotImplementedException();
+    }
 }

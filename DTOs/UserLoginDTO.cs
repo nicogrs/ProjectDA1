d3112ea5ca41c76b35536150e3cd0d7b6ae6270d
Florencia <FB243045@fi365.or.edu.uy>
2024-10-12 19:53:08 -0300

    RED: Falla prueba ToEntity de UserLoginDTO

---
+++ DTOs/UserLoginDTO.cs
@@ -0,0 +1,20 @@
+﻿using Dominio;
+namespace DTOs;
+
+public class UserLoginDTO
+{
+    public string Email { get; set; }
+    
+    public string Password { get; set; }
+    
+    public User ToEntity()
+    {
+        return new User();
+    }
+    
+    public static UserLoginDTO FromEntity(User u)
+    {
+        return new UserLoginDTO();
+        
+    }
+}
---
+++ Test/DTOsTest/UserLoginDTOTest.cs
@@ -0,0 +1,30 @@
+﻿namespace Test;
+using DTOs;
+using Dominio;
+
+[TestClass]
+public class UserLoginDTOTest
+{
+    private UserLoginDTO _userLoginDTO;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+        _userLoginDTO = new UserLoginDTO
+        {
+            Email = "juanpe@gmail.com",
+            Password = "TestPass$1",
+        };
+    }
+
+    [TestMethod]
+    public void ToEntity()
+    {
+        var user = _userLoginDTO.ToEntity();
+        
+        Assert.IsNotNull(user);
+        Assert.AreEqual(user.Email, _userLoginDTO.Email);
+        Assert.AreEqual(user.Password, _userLoginDTO.Password);
+    }
+    
+}

44ed8c73653cb4082c334fa95168cd699888424f
rpresno <rjpresno@gmail.com>
2024-11-10 13:33:48 -0300

    GREEN: EpicCreate Test

--- Dominio/Models/Epic.cs
+++ Dominio/Models/Epic.cs
@@ -2,17 +2,19 @@ namespace Dominio;
 
 public class Epic
 {
+
     public string Title { get; set; }
-    public Priority Priority { get; set; }
+    public Precedence Priority { get; set; }
     public string Description { get; set; }
     public DateTime ExpirationDate { get; set; }
     public List<Task> Tasks { get; set; }
-    
-    
-    public enum Priority
+    public int TaskCount { get; set; }
+
+
+    public enum Precedence
     {
-        Low,
-        Medium,
+        Low ,
+        Medium ,
         Urgent
     }
     
--- Test/ModelsTest/EpicTest.cs
+++ Test/ModelsTest/EpicTest.cs
@@ -1,3 +1,6 @@
+using Dominio;
+using Task = System.Threading.Tasks.Task;
+
 namespace Test;
 
 [TestClass]
@@ -9,25 +12,25 @@ public class EpicTest
     [TestInitialize]
     public void Setup()
     {
-        tasks = new List<Task>();
-        
+        tasks = new List<Task>(){};
+
         e = new Epic()
         {
             Title = "Titulo de epica",
-            Priority = Epic.Priority.Low,
+            Priority = Epic.Precedence.Low,
             Description = "Descripcion de la epica",
-            ExpirationDate = new DateTime(2030,1,1),
-            Tasks = tasks
-        }
+            ExpirationDate = new DateTime(2030, 1, 1),
+            Tasks = null
+        };
     }
     
     [TestMethod]
     public void EpicCreate()
     {
         Assert.AreEqual(e.Title, "Titulo de epica");
-        Assert.AreEqual(e.Priority,Epic.Priority.Low);
+        Assert.AreEqual(e.Priority,Epic.Precedence.Low);
         Assert.AreEqual(e.Description, "Descripcion de la epica");
         Assert.AreEqual(e.ExpirationDate, new DateTime(2030,1,1));
-        Assert.AreEqual(e.Tasks, tasks);
+        Assert.IsNull(e.Tasks);
     }
 }

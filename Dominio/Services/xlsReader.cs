a9e7f108f29b7c7a788527b846468cb6365e2bab
rpresno <rjpresno@gmail.com>
2024-11-11 21:26:56 -0300

    REFACTOR task import en proceso

--- Dominio/Services/xlsReader.cs
+++ Dominio/Services/xlsReader.cs
@@ -1,6 +1,39 @@
 namespace Dominio.Services;
+using System;
+using ClosedXML.Excel;
 
-public class xlsReader : TaskImportService
+public class XlsReader : TaskImportService
 {
-    
+    public List<string> ConvertXlsFileContentToCsv(string filePath)
+    {
+        List<string> lines = new List<string>();
+        
+        // Ensure the file exists
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("File not found.");
+            return lines;
+        }
+        
+        // Load the workbook
+        using (var workbook = new XLWorkbook(filePath))
+        {
+            var worksheet = workbook.Worksheet(1);
+            var rows = worksheet.RangeUsed().RowsUsed().Skip(1); // Skip the header row
+
+            foreach (var row in rows)
+            {
+                string line = "";
+                
+                for (int i = 0; i < 5; i++)
+                {
+                  line += row.Cell(i).Value.ToString();  
+                }
+
+                lines.Add(line);
+            }
+        }
+        
+        return lines;
+    }
 }

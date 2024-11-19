846d69935ffbf485785f857a4eee00dc41f005e0
Florencia <FB243045@fi365.ort.edu.uy>
2024-11-19 20:48:13 -0300

    REFACTOR: se modificn test para que vuelvan a pasar

--- DTOs/UserDTOs/UserModifyDTO.cs
+++ DTOs/UserDTOs/UserModifyDTO.cs
@@ -11,13 +11,18 @@ public class UserModifyDTO
     [StringLength(100, MinimumLength = 2, ErrorMessage = "El apellido debe tener entre 2 y 100 caracteres")]
     public string Surname { get; set; }
 
+    
+    [Range(typeof(DateTime), "01/01/1900", "12/12/2023", ErrorMessage = "La fecha de nacimiento debe ser menor al año actual.")]
     [DataType(DataType.Date)]
     public DateTime BirthDate { get; set; }
     
+    [EmailAddress(ErrorMessage = "Formato de email inválido")]
     public string Email { get; set; }
     
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=]).{8,}$", 
+        ErrorMessage = "La contraseña debe tener al menos 8 caracteres, incluir una mayúscula, una minúscula, un número y un carácter especial (@, #, $, etc.).")]
     public string Password { get; set; }
-    public bool Admin { get; set; }
+  
     
     public User ToEntity()
         {
@@ -28,7 +33,6 @@ public class UserModifyDTO
                 BirthDate = this.BirthDate,
                 Email = this.Email,
                 Password = this.Password,
-                Admin = this.Admin
             };
         }
 }
--- Test/DTOsTest/UserModifyDTOTest.cs
+++ Test/DTOsTest/UserModifyDTOTest.cs
@@ -1,10 +1,14 @@
-﻿namespace Test.DTOsTest;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test.DTOsTest;
 using DTOs;
 
 [TestClass]
 public class UserModifyDTOTest
 {
     private UserModifyDTO _userModifyDTO;
+    private UserModifyDTO validUser;
+    private UserModifyDTO invalidUser;
 
     [TestInitialize]
     public void SetUp()
@@ -16,6 +20,24 @@ public class UserModifyDTOTest
             Email = "sofi@gmail.com",
             BirthDate = new DateTime(1988, 2, 28),
             Password = "TestPass$1",
+        };
+        
+        validUser = new UserModifyDTO
+        {
+            Name = "Juan",
+            Surname = "Perez",
+            Email = "juanpe@gmail.com",
+            BirthDate = new DateTime(1999, 6, 14),
+            Password = "TestPass$1"
+        };
+        
+        invalidUser = new UserModifyDTO
+        {
+            Name = "J", 
+            Surname = "", 
+            BirthDate = new DateTime(1890, 1, 1), 
+            Email = "juan.perez", 
+            Password = "pass" 
         };
     }
 
@@ -31,5 +53,30 @@ public class UserModifyDTOTest
         Assert.AreEqual(user.BirthDate, _userModifyDTO.BirthDate);
         Assert.AreEqual(user.Password, _userModifyDTO.Password);
     }
+    
+    [TestMethod]
+    public void ValidUser()
+    {
+        var validationResults = new List<ValidationResult>();
+        var validationContext = new ValidationContext(validUser);
+        
+        bool isValid = Validator.TryValidateObject(validUser, validationContext, validationResults, true);
+        
+        Assert.IsTrue(isValid);
+        Assert.AreEqual(0, validationResults.Count);
+    }
+
+    [TestMethod]
+    public void InvalidUser()
+    {
+        var validationResults = new List<ValidationResult>();
+        var validationContext = new ValidationContext(invalidUser);
+        
+        bool isValid = Validator.TryValidateObject(invalidUser, validationContext, validationResults, true);
+        
+        Assert.IsFalse(isValid);
+        Assert.IsTrue(validationResults.Count > 0);
+    }
+
     
 }
